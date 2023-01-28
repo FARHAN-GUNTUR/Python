5a3860a5e8ae9f2f0c05e37dfa3aa8be316a0b67
Guntur Wahyu Septiaji <farhanguntur35@gmail.com>
2023-01-28 22:47:50 +0700

    Menambahkan operator logika or dan and. Serta mencoba latihan soal

--- main.py
+++ main.py
@@ -1,10 +1,65 @@
-#CASTING TIPE DATA
-dataFloat = 5.9
-dataInt = int(dataFloat)
-dataBool = bool(dataFloat)
-dataStr = str(dataFloat)
-print("data = ", dataFloat, "merupakan tipe data", type(dataFloat))
-print("data = ", dataInt, "merupakan tipe data", type(dataInt)) #DIBULATKAN KE BAWAH
-print("data = ", dataBool, "merupakan tipe data", type(dataBool))
-print("data = ", dataStr, "merupakan tipe data", type(dataStr))
-print("=" * 25);
+# !CASTING TIPE DATA
+# *dataFloat = 5.9
+# *dataInt = int(dataFloat)
+# *dataBool = bool(dataFloat)
+# *dataStr = str(dataFloat)
+# *print("data = ", dataFloat, "merupakan tipe data", type(dataFloat))
+# *print("data = ", dataInt, "merupakan tipe data", type(dataInt)) #DIBULATKAN KE BAWAH
+# *print("data = ", dataBool, "merupakan tipe data", type(dataBool))
+# *print("data = ", dataStr, "merupakan tipe data", type(dataStr))
+# *print("=" * 25);
+
+# !OPERATOR LOGIKA BITWISE (XOR) (MENGHASILKAN NILAI TRUE JIKA SALAH SATU BERNILAI TRUE)
+# *a = False
+# *b = False
+# *c = a ^ b
+# *print(a, 'XOR', b, '=', c)
+# *a = True
+# *b = False
+# *c = a ^ b
+# *print(a, 'XOR', b, '=', c)
+# *a = False
+# *b = True
+# *c = a ^ b
+# *print(a, 'XOR', b, '=', c)
+# *a = True
+# *b = True
+# *c = a ^ b
+# *print(a, 'XOR', b, '=', c)
+# *print('=' * 25)
+
+# !OPERATOR LOGIKA OR (||) (AKAN MENGHASILKAN NILAI TRUE APABILA SALAH SATUNYA TRUE )
+# *inputUser = float(input('Masukkan angka kurang dari 5 atau lebih dari 10 : '))
+# *kurangDari = (inputUser < 5)
+# *lebihDari = (inputUser > 10)
+# *hasil = kurangDari or lebihDari
+# *print(hasil);
+# *print('=' * 25)
+
+# !OPERATOR LOGIKA AND (&&) (MENGHASILKAN NILAI TRUE APABILA SEMUANYA BERNILAI TRUE)
+# *inputUser = float(input('Masukkan angka lebih dari 5 dan kurang dari 10 : '))
+# *lebihDari = inputUser > 5
+# *kurangDari = inputUser < 10
+# *hasil = lebihDari and kurangDari
+# *print(hasil);
+# *print('=' * 25)
+
+# ?LATIHAN OPERATOR LOGIKA
+# !NOMER 1
+# *inputUser = float(input('Masukkan angka : '))
+# *lebihDari = (inputUser >= 0 and inputUser <= 5)
+# *print('lebih dari = ', lebihDari);
+# *kurangDari = (inputUser >= 8 and inputUser <= 11)
+# *print('kurang dari = ' , kurangDari)
+# *hasil = lebihDari or kurangDari
+# *print(hasil);
+# *print('=' * 25)
+
+# !NOMER 2
+# *inputUser = float(input('Masukkan angka : '))
+# *kurangDari = (inputUser <= 0 or inputUser >= 11)
+# *print('kurang dari = ', kurangDari)
+# *lebihDari = (inputUser >= 5 and inputUser <= 8)
+# *print('lebih dari = ', lebihDari)
+# *hasil = kurangDari or lebihDari
+# *print(hasil);
