562f9f5c79df3223c5c86672f6007afbc9dc3550
Guntur Wahyu Septiaji <farhanguntur35@gmail.com>
2023-02-24 09:47:02 +0700

    ok

---
+++ for loop.py
@@ -0,0 +1,6 @@
+#
+# ! 1. FOR LOOP ZIP
+nama = ["Farhan", "Guntur", "Wahyu", "Septiaji"]
+waktu = ["Pagi", "Siang", "Sore", "Malam"]
+for name, time in zip(nama, waktu):
+    print(f"Halo {name}, Selamat {time}")
